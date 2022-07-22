6ec127bff490e41a6d30e476a155a55b0b80fbf5
vvbereziuk <bereziuk.vg@gmail.com>
2022-07-22 21:17:09 +0300

    hw1

---
+++ lesson1/hw1/main.js
@@ -0,0 +1,93 @@
+let a1 = 'Hello';
+console.log(a1);
+// alert(a1);
+document.write(a1);
+
+let a2 = 'owu';
+console.log(a2);
+// alert(a2);
+document.write(a2);
+
+let a3 = 'com';
+console.log(a3);
+// alert(a3);
+document.write(a3);
+
+let a4 = 'ua';
+console.log(a4);
+// alert(a4);
+document.write(a4);
+
+let n1 = 1;
+console.log(n1);
+// alert(n1);
+document.write(n1);
+
+let n2 = 10;
+console.log(n2);
+// alert(n2);
+document.write(n2);
+
+let n3 = -999;
+console.log(n3);
+// alert(n3);
+document.write(n3);
+
+let n4 = 123;
+console.log(n4);
+// alert(n4);
+document.write(n4);
+
+let p1 = 3.14;
+console.log(p1);
+// alert(p1);
+document.write(p1)
+
+let p2 = 2.7;
+console.log(p2);
+// alert(p2);
+document.write(p2);
+
+let p3 = 16;
+console.log(p3);
+// alert(p3);
+document.write(p3);
+
+let b1 = (!!' ');
+console.log(b1);
+// alert(b1);
+document.write(b1);
+
+let b2 = (!!'');
+console.log(b2);
+// alert(b2);
+document.write(b2);
+
+let firstName = 'Valentyn';
+let middleName = 'Vitaliiovych';
+let lastName = 'Bereziuk';
+let person = firstName +' '+ middleName +' '+ lastName;
+console.log(person);
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof (a));
+console.log(typeof (b));
+console.log(typeof (c));
+
+let name = prompt('What is your first name?');
+let midname = prompt('What is your middle name?');
+let age = prompt('How old are you?');
+console.log(name, midname, age);
+
+
+
+
+
+
+
+
+
+
+
